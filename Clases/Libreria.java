eea21c7588b970d74178a712e9636bea23a1941a
Arturo Mory Jara <morylex@hotmail.com>
2023-04-23 22:23:48 -0500

    medoto ordenar arreglos

--- Clases/Libreria.java
+++ Clases/Libreria.java
@@ -23,4 +23,28 @@ public class Libreria {
         }while (!sc.hasNextLine());*/
         return cadena;
     }
+    public int[] OrdenarArreglo(int nelementos){
+        int arreglo[], posicion, aux;
+
+        //numElementos = Integer.parseInt(JOptionPane.showInputDialog("Insertar numero de elementos"));
+        arreglo = new int[nelementos];
+
+        //JOptionPane.showMessageDialog(null, "Insertar valores de los elementos en el arreglo");
+        //for (int i = 0; i < numElementos; i++) {
+        //    arreglo[i] = Integer.parseInt(JOptionPane.showInputDialog("Insertar valor del elemento " + (i + 1) + ": "));
+        //}
+
+        //Ordenación por inserción
+        for (int i = 0; i < nelementos; i++) {
+            posicion = i;
+            aux = arreglo[i];
+
+            while ((posicion > 0 && (arreglo[posicion - 1] > aux))) {
+                arreglo[posicion] = arreglo[posicion - 1];
+                posicion--;
+            }
+            arreglo[posicion] = aux;
+        }
+        return arreglo;
+    }
 }
